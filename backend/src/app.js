b80917fbb27089c2ab238d607a89aebcde357802
Saiful Alom <saifulalom49590@gmail.com>
2025-12-25 13:11:19 +0600

    refactor: reorganize imports in app.js for better readability

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,22 @@
 // src/app.js
-
-import './load-docs.js'
 import express from 'express';
+
+// error handling
 import { errorHandler } from './common/middleware/error-handler.js';
+import { notFoundHandler } from './common/middleware/not-found.js';
+
+// swagger api documetaion
+import './load-docs.js'
+import swaggerUI from 'swagger-ui-express'
+import { generateOpenApiSpec } from './common/docs/generate-openapi.js';
+
+// database
 import { connectDB } from './common/utils/connect-db.js';
+
+// routes
 import authRoutes from './modules/auth/auth.routes.js';
 import assetsRoutes from './modules/assets/asset.routes.js';
 import categoriesRoutes from './modules/categories/category.routes.js';
-import { notFoundHandler } from './common/middleware/not-found.js';
-import swaggerUI from 'swagger-ui-express'
-import { generateOpenApiSpec } from './common/docs/generate-openapi.js';
 
 const app = express();
 
