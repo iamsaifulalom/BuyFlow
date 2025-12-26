4d559a4253be8f4280221a764a8f7abcb0a52f65
Saiful Alom <saifulalom49590@gmail.com>
2025-12-26 17:52:59 +0600

    feat: hide default browser scrollbar

--- frontend/src/shared/components/sidebar.tsx
+++ frontend/src/shared/components/sidebar.tsx
@@ -53,6 +53,7 @@ export function Sidebar({ children }: { children?: ReactNode }) {
         <>
             <div className={cn(
                 'fixed w-65 z-20 bg-background transition-all duration-300 overflow-y-auto top-0 left-0 h-screen',
+                "[scrollbar-width:none] [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden",
                 isSidebarOpen ? "translate-x-0" : "-translate-x-full"
             )}>
                 {children}
@@ -62,13 +63,14 @@ export function Sidebar({ children }: { children?: ReactNode }) {
             <div onClick={toggleSidebar}
                 className={cn(
                     'fixed w-full bg-foreground/10 backdrop-blur-sm transition-all duration-300 overflow-y-auto top-0 left-0 h-screen',
-                    isSidebarOpen ? "translate-x-0" : "-translate-x-full"
+                    "[scrollbar-width:none] [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden",
+                    isSidebarOpen ? "translate-x-0" : "-translate-x-full",
                 )} />
         </>
     )
 
     return (
-        <aside className="z-30 overflow-y-auto h-screen w-75 shrink-0 border-r hidden lg:block bg-white ease-in-out">
+        <aside className="z-30 [scrollbar-width:none] [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden overflow-y-auto h-screen w-75 shrink-0 border-r hidden lg:block bg-white ease-in-out">
             {children}
         </aside>
     )
@@ -79,7 +81,7 @@ export function Sidebar({ children }: { children?: ReactNode }) {
 export function SidebarInset({ children }: { children?: ReactNode }) {
 
     return (
-        <main className="h-screen overflow-y-auto duration-300 ease-in-out">
+        <main className="h-screen overflow-y-auto [scrollbar-width:none] [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden duration-300 ease-in-out">
             {children}
         </main>
     )
